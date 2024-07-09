fa240834117359816214325396b09929ee29ece5
ChouguleAnkita13 <flutterankita@gmail.com>
2024-07-09 17:40:06 +0530

    added sqflite and path

---
+++ lib/model/user_model.dart
@@ -0,0 +1,12 @@
+class User {
+  String name;
+  String email;
+  String password;
+  String confirm;
+
+  User(
+      {required this.name,
+      required this.email,
+      required this.password,
+      required this.confirm});
+}
